e9ad30f921744d72f40cdf06da6bd98ca0a38277
McTambu <djennymasengidbanj@gmail.com>
2020-05-25 05:49:44 +0100

    Menu principal

--- AppPadaria/Menu.cs
+++ AppPadaria/Menu.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace AppPadaria
 {
-    public partial class Form1 : Form
+    public partial class Menu : Form
     {
-        public Form1()
+        public Menu()
         {
             InitializeComponent();
             //APP criado pela a WAT company
